2710f752c6402a624163f4cc318f19cae4c8526f
Tuncayonurkaan <e170501011@stud.tau.edu.tr>
2020-11-06 13:09:52 +0300

    örnek1

--- app/src/main/java/com/example/plansplit/MainActivity.java
+++ app/src/main/java/com/example/plansplit/MainActivity.java
@@ -44,7 +44,9 @@ public class MainActivity extends AppCompatActivity {
         });
         // Configure sign-in to request the user's ID, email address, and basic
         // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
                 .requestEmail()
                 .build();
 
