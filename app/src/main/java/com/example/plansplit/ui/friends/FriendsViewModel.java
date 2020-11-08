5c3cb3e3f51b36e95af70618d79e2d89d8f090c0
sait <e160501132@stud.tau.edu.tr>
2020-11-09 01:07:38 +0300

    genel isimlendirmeler düzenlendi. string, color, font dosyaları düzenlendi. item_notificaiton.xml tamamlandı. arka plan eklendi. program sorunsuz çalışır durumda. sait

--- app/src/main/java/com/example/plansplit/ui/friends/FriendsViewModel.java
+++ app/src/main/java/com/example/plansplit/ui/friends/FriendsViewModel.java
@@ -1,14 +1,14 @@
-package com.example.plansplit.ui.dashboard;
+package com.example.plansplit.ui.friends;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class DashboardViewModel extends ViewModel {
+public class FriendsViewModel extends ViewModel {
 
     private MutableLiveData<String> mText;
 
-    public DashboardViewModel() {
+    public FriendsViewModel() {
         mText = new MutableLiveData<>();
         mText.setValue("This is dashboard fragment");
     }
--- app/src/main/java/com/example/plansplit/ui/personal/PersonalViewModel.java
+++ app/src/main/java/com/example/plansplit/ui/personal/PersonalViewModel.java
@@ -1,14 +1,14 @@
-package com.example.plansplit.ui.home;
+package com.example.plansplit.ui.personal;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class HomeViewModel extends ViewModel {
+public class PersonalViewModel extends ViewModel {
 
     private MutableLiveData<String> mText;
 
-    public HomeViewModel() {
+    public PersonalViewModel() {
         mText = new MutableLiveData<>();
         mText.setValue("This is home fragment");
     }
