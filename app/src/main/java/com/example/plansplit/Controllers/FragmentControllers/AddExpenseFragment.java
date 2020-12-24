68ee81c682383f861f64de44c5bd68245a1d7ec2
sait <e160501132@stud.tau.edu.tr>
2020-12-24 23:37:28 +0300

    Db yapısı tek constructorlı hale getirilip iyileştirmeler sağlandı

--- app/src/main/java/com/example/plansplit/Controllers/FragmentControllers/AddExpenseFragment.java
+++ app/src/main/java/com/example/plansplit/Controllers/FragmentControllers/AddExpenseFragment.java
@@ -1,17 +1,11 @@
 package com.example.plansplit.Controllers.FragmentControllers;
 
-//public class FriendExpenseFragment {
-//}
-
-
 import android.app.Dialog;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -24,12 +18,8 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.Toast;
-import com.example.plansplit.Controllers.FragmentControllers.friends.FriendExpenseNoteDialog;
-import com.example.plansplit.Models.Objects.Person;
 import com.example.plansplit.R;
-import com.example.plansplit.Controllers.Adapters.CustomDialogAdapter;
 import com.example.plansplit.Controllers.FragmentControllers.ShareMethod.ShareMethodFriendsFragment;
-
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
@@ -49,7 +39,6 @@ public class AddExpenseFragment extends Fragment {
     public static Button shareBtn;
     private Button dpokBtn;
     private String payer_name;
-    private String note;
     private String date;
     private Button expensetypeBtn;
     private Button dpexpenseexitBtn;
@@ -63,23 +52,13 @@ public class AddExpenseFragment extends Fragment {
     private ImageView expensePicture;
     private int expensePictureResourceID;
     private int foodPicture,wearPicture,hygienePicture,stationeryPicture, otherPicture;
-    private ArrayList myImageNameListFriend;
-
-    public static AddExpenseFragment newInstance() {
-        return new AddExpenseFragment();
-    }
+    boolean ctrlDate=false;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
         final View root = inflater.inflate(R.layout.fragment_expense, container, false);
-
-        myImageNameListFriend = new ArrayList<>();
-
-        myImageNameListFriend.add(new Person("veli", R.drawable.denemeresim, 0));
-        myImageNameListFriend.add(new Person("veli", R.drawable.denemeresim, 0));
-        myImageNameListFriend.add(new Person("ali", R.drawable.denemeresim, 0));
-        myImageNameListFriend.add(new Person("sami", R.drawable.denemeresim, 0));
+        ctrlDate=false;
 
         edittextexpensename=root.findViewById(R.id.editTextExpenseName);
         edittextexpenseamounth=root.findViewById(R.id.editTextExpenseAmounth);
@@ -90,29 +69,8 @@ public class AddExpenseFragment extends Fragment {
         noteBtn = root.findViewById(R.id.noteButton);
         shareBtn = root.findViewById(R.id.method_button);
         expensetypeBtn=root.findViewById(R.id.expense_type_button);
-        noteBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                openNoteDialog();
-            }
-        });
         date=new SimpleDateFormat("dd/MM/yyyy", Locale.getDefault()).format(new Date());
         calenderBtn.setText(date);
-
-        /*dialogBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                openPayerDialog(FriendExpenseFragment.this);
-            }
-        });*/
-        /*shareBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                openShareMethodDialog();
-
-            }
-        });*/
-
 
         calenderBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -212,16 +170,15 @@ public class AddExpenseFragment extends Fragment {
                 System.out.println(date);   //harcama tarihi
                 System.out.println(expensePictureResourceID);  //harcama resmi idsi
 
+
+
+
+
+
             }
         });
 
         return root;
-
-    }
-
-    public void openNoteDialog() {
-        FriendExpenseNoteDialog friendExpenseNoteDialog = new FriendExpenseNoteDialog();
-        friendExpenseNoteDialog.show(getParentFragmentManager(), "friend note dialog");
 
     }
 
@@ -252,28 +209,10 @@ public class AddExpenseFragment extends Fragment {
                 dialog.dismiss();
             }
         });
-    /*
-            RecyclerView recyclerView = dialog.findViewById(R.id.recyclerPayer);
-            CustomDialogAdapter adapterRe = new CustomDialogAdapter(getContext(), myImageNameList);
-            recyclerView.setAdapter(adapterRe);
-            recyclerView.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL, false));
-
-            recyclerView.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-
-                }
-            });
-
-     */
-
         dialog.show();
 
     }
 
-    public void setNote(String note) {
-        this.note = note;
-    }
 
 
     public String getPayer_name() {
@@ -297,6 +236,7 @@ public class AddExpenseFragment extends Fragment {
         dpokBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                ctrlDate=true;
                 startYear = dp.getYear();
                 startMonth = dp.getMonth() + 1;
                 startDay = dp.getDayOfMonth();
@@ -317,35 +257,8 @@ public class AddExpenseFragment extends Fragment {
             @Override
             public void onClick(View view) {
 
-               /* PopupMenu popup = new PopupMenu(getContext(), dpmenuBtn);
-                //popup.getMenuInflater().inflate(R.menu.date_picker_menu,popup.getMenu());
-                popup.inflate(R.menu.date_picker_menu);
-                popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
-                    @Override
-                    public boolean onMenuItemClick(MenuItem menuItem) {
-                        switch (menuItem.getItemId()) {
-                            case R.id.date_picker_menu_for_once:
-                                repetition = getResources().getString(R.string.date_picker_menu_for_once);
-                                break;
-                            case R.id.date_picker_menu_weekly_repeat:
-                                repetition = getResources().getString(R.string.date_picker_menu_weekly_repeat);
-                                break;
-                            case R.id.date_picker_menu_monthly_repeat:
-                                repetition = getResources().getString(R.string.date_picker_menu_monthly_repeat);
-                                break;
-                            case R.id.date_picker_menu_yearly_repeat:
-                                repetition = getResources().getString(R.string.date_picker_menu_yearly_repeat);
-                                break;
-                        }
-                        dpmenuBtn.setText(repetition);
-                        return false;
-                    }
-                });
-                popup.show();*/
             }
         });
-
-
 
         dialog.show();
     }
