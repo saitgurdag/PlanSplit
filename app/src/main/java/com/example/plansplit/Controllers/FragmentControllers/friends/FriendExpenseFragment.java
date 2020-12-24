6082e895fae5f624619c4d3f8d9168c3355f76ba
Tuncayonurkaan <e170501011@stud.tau.edu.tr>
2020-12-24 13:48:50 +0300

    grup harcamaları sayfası için filtre eklendi. Harcamalar ekranında gerekli düzenlemeler yapıldı.

--- app/src/main/java/com/example/plansplit/Controllers/FragmentControllers/friends/FriendExpenseFragment.java
+++ app/src/main/java/com/example/plansplit/Controllers/FragmentControllers/friends/FriendExpenseFragment.java
@@ -19,9 +19,15 @@ import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.PopupMenu;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.example.plansplit.Controllers.Adapters.CustomDialogAdapter;
 
@@ -31,7 +37,10 @@ import com.example.plansplit.Models.Objects.Person;
 import com.example.plansplit.R;
 import com.example.plansplit.Controllers.FragmentControllers.ShareMethod.ShareMethodFriendsFragment;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.Locale;
 
 public class FriendExpenseFragment extends Fragment {
 
@@ -49,6 +58,18 @@ public class FriendExpenseFragment extends Fragment {
     private String payer_name;
     private String note;
     private String date;
+    private Button expensetypeBtn;
+    private Button dpexpenseexitBtn;
+    private Button dpexpenseokBtn;
+    private EditText edittextexpensename;
+    private EditText edittextexpenseamounth;
+    private Button saveexpenseBtn;
+    private String expensename;
+    private String expenseamounth;
+    private String expenseType;
+    private ImageView expensePicture;
+    private int expensePictureResourceID;
+    private int foodPicture,wearPicture,hygienePicture,stationeryPicture, otherPicture;
     private ArrayList myImageNameListFriend;
 
     public static FriendExpenseFragment newInstance() {
@@ -67,36 +88,136 @@ public class FriendExpenseFragment extends Fragment {
         myImageNameListFriend.add(new Person("ali", R.drawable.denemeresim, 0));
         myImageNameListFriend.add(new Person("sami", R.drawable.denemeresim, 0));
 
+        edittextexpensename=root.findViewById(R.id.editTextExpenseName);
+        edittextexpenseamounth=root.findViewById(R.id.editTextExpenseAmounth);
+        saveexpenseBtn=root.findViewById(R.id.saveExpenseButton);
+
         dialogBtn = root.findViewById(R.id.payer_button);
         calenderBtn = root.findViewById(R.id.calendarButton);
         noteBtn = root.findViewById(R.id.noteButton);
         shareBtn = root.findViewById(R.id.method_button);
+        expensetypeBtn=root.findViewById(R.id.expense_type_button);
         noteBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 openNoteDialog();
             }
         });
+        date=new SimpleDateFormat("dd/MM/yyyy", Locale.getDefault()).format(new Date());
+        calenderBtn.setText(date);
 
-        dialogBtn.setOnClickListener(new View.OnClickListener() {
+        /*dialogBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 openPayerDialog(FriendExpenseFragment.this);
             }
-        });
-        shareBtn.setOnClickListener(new View.OnClickListener() {
+        });*/
+        /*shareBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 openShareMethodDialog();
 
             }
-        });
+        });*/
 
 
         calenderBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 showDatePicker();
+
+            }
+        });
+
+        expensetypeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog = new Dialog(getContext(), R.style.Theme_AppCompat_DayNight_Dialog_MinWidth);
+                dialog.setCancelable(false);
+                dialog.setContentView(R.layout.dialog_select_expense_type);
+                dialog.show();
+                dpexpenseokBtn=dialog.findViewById(R.id.dpExpenseOKButton);
+                dpexpenseexitBtn=dialog.findViewById(R.id.dpExpenseExitButton);
+                dpexpenseexitBtn.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        dialog.dismiss();
+                    }
+                });
+                dpexpenseokBtn.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        dialog.dismiss();
+                        expensetypeBtn.setText(expenseType);
+                    }
+                });
+                Spinner expensespinner=dialog.findViewById(R.id.spinnerExpense);
+                expensePicture=dialog.findViewById(R.id.ImageExpense);
+                foodPicture = R.drawable.ic_baseline_fastfood_24;
+                wearPicture = R.drawable.ic_baseline_wear_24;
+                hygienePicture = R.drawable.ic_baseline_hygiene_24;
+                stationeryPicture = R.drawable.ic_baseline_school_24;
+                otherPicture = R.drawable.ic_other;
+                ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getContext(),
+                        R.array.expenseSpinnerItems, android.R.layout.simple_spinner_item);
+                adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                expensespinner.setAdapter(adapter);
+                expensespinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                    @Override
+                    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                        String selectedtext=adapterView.getItemAtPosition(i).toString();
+                        switch (i){
+                            case 0:
+                                expensePictureResourceID=foodPicture;
+
+                                break;
+                            case 1:
+                                expensePictureResourceID=wearPicture;
+
+                                break;
+                            case 2:
+                                expensePictureResourceID=stationeryPicture;
+
+                                break;
+                            case 3:
+                                expensePictureResourceID=hygienePicture;
+
+                                break;
+                            case 4:
+                                expensePictureResourceID=otherPicture;
+
+                                break;
+                        }
+                        expensePicture.setImageResource(expensePictureResourceID);
+                        expenseType=selectedtext;
+                    }
+
+
+                    @Override
+                    public void onNothingSelected(AdapterView<?> adapterView) {
+
+                    }
+                });
+
+
+            }
+        });
+
+        saveexpenseBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                expensename=edittextexpensename.getText().toString();
+                expenseamounth=edittextexpenseamounth.getText().toString();
+                if(!android.text.TextUtils.isDigitsOnly(expenseamounth)){
+                    Toast.makeText(getContext(), "Hatalı girdi", Toast.LENGTH_LONG).show();
+                }
+                else{
+                    System.out.println(expenseamounth); //harcama miktarı
+                }
+                System.out.println(expensename);  //harcama ismi
+                System.out.println(expenseType);  //harcama tipi
+                System.out.println(date);   //harcama tarihi
+                System.out.println(expensePictureResourceID);  //harcama resmi idsi
 
             }
         });
@@ -205,7 +326,7 @@ public class FriendExpenseFragment extends Fragment {
             @Override
             public void onClick(View view) {
 
-                PopupMenu popup = new PopupMenu(getContext(), dpmenuBtn);
+               /* PopupMenu popup = new PopupMenu(getContext(), dpmenuBtn);
                 //popup.getMenuInflater().inflate(R.menu.date_picker_menu,popup.getMenu());
                 popup.inflate(R.menu.date_picker_menu);
                 popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
@@ -229,7 +350,7 @@ public class FriendExpenseFragment extends Fragment {
                         return false;
                     }
                 });
-                popup.show();
+                popup.show();*/
             }
         });
         dialog.show();
